68ddb77217d56dadb513b8f46dd01171fc686274
jon4st <45789952+jon4st@users.noreply.github.com>
2020-03-03 11:54:40 +0200

    Pamirsau ka padariau, bet reikia ispushint :D

--- frontend/user-side/src/components/App/App.js
+++ frontend/user-side/src/components/App/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import FixedNavbar from '../FixedNavbar/FixedNavbar';
+import Header from '../Header/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+        <FixedNavbar />
+        <Header />
     </div>
   );
 }
